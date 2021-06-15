7768e2f8b80a73d62f1c32527534896a6a25f2cd
shubhsjain <svj8866@gmail.com>
2021-06-15 16:18:21 +0530

    globals

---
+++ 2-globals.js
@@ -0,0 +1,12 @@
+//  GLOBALS     - NO WINDOW !
+
+// __dirname    - path to current directory.
+// __filename   - file name.
+// require      - function to use modules (commonJS).
+// module       - info about current module (file).
+// process      - info about the envirnment where the program is executed.
+
+console.log(__dirname)
+setInterval(() => {
+    console.log('Hello World')
+}, 1000)
