f1cc137a08450f42764dd9e07dbeed87508cfbb4
shubhsjain <svj8866@gmail.com>
2021-06-15 19:42:19 +0530

    alternative syntax

--- app.js
+++ app.js
@@ -2,8 +2,12 @@
 // Modules  - Encapsulated code (only share minimun)
 const names = require('./4-names')
 const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+
 // console.log(names)
 
 sayHi('Sanvi')
 sayHi(names.lucky)
 sayHi(names.shubh)
+
+console.log(data)
