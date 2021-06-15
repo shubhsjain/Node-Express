6af3f1ff12902f7969d9fecf34468f2b350f5ef7
shubhsjain <svj8866@gmail.com>
2021-06-15 20:36:26 +0530

    modules

---
+++ 3-module.js
@@ -0,0 +1,13 @@
+// CommonJS, every file is a module (by default)
+// Modules  - Encapsulated code (only share minimun)
+const names = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+
+// console.log(names)
+// console.log(data)
+
+// sayHi('Sanvi')
+// sayHi(names.lucky)
+// sayHi(names.shubh)
