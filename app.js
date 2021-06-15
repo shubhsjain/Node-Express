a900ecfe27fcdfc33caf5e89504891a344542b05
shubhsjain <svj8866@gmail.com>
2021-06-15 20:31:24 +0530

    export funtion ( mind grenade )

--- app.js
+++ app.js
@@ -3,11 +3,11 @@
 const names = require('./4-names')
 const sayHi = require('./5-utils')
 const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
 
 // console.log(names)
+// console.log(data)
 
-sayHi('Sanvi')
-sayHi(names.lucky)
-sayHi(names.shubh)
-
-console.log(data)
+// sayHi('Sanvi')
+// sayHi(names.lucky)
+// sayHi(names.shubh)
