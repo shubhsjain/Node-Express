bfe1def66ff86291f75054e21643e337510aa736
shubhsjain <svj8866@gmail.com>
2021-06-19 21:42:19 +0530

    nodemon

--- app.js
+++ app.js
@@ -17,3 +17,11 @@ const _ = require("lodash");
 const items = [1, [2, [3, [4, [5]]]]];
 const newItems = _.flattenDeep(items);
 console.log(newItems);
+
+// DEV DEPENEDENCIES
+// NODEMON  = ITS THE DEVDEPENDENCIEY WHICH RESTARTS OUR PROJECT
+// npm i nodemon -D
+
+// to run dev nodemon - npm run dev
+
+console.log("hello world!");
