3e06dc25471fe72ece0ebf6a2696e6b9c08e0e30
shubhsjain <svj8866@gmail.com>
2021-07-04 19:11:23 +0530

    express basic

---
+++ 2-Express/final/03-express-basics.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("user hit the resource");
+  res.status(200).send("Home Page");
+});
+
+app.get("/about", (req, res) => {
+  res.status(200).send("About Page");
+});
+
+app.all("*", (req, res) => {
+  res.status(404).send("<h1>resource not found</h1>");
+});
+
+app.listen(5000, () => {
+  console.log("server is listening on port 5000...");
+});
+
+// app.get      /**read data */
+// app.post     /**insert data */
+// app.put      /**update data */
+// app.delete   /**delete date */
+// app.all
+// app.use
+// app.listen   /**seerver listining */
